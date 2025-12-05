f59f031a3e588b5eeb771be03f1284ca2dfc7ec7
Anders <anders.sovik@outlook.com>
2025-12-05 18:23:48 +0100

    Started the logic fikk begynt på litt av forgrunnen va loggiken

---
+++ logikk.js
@@ -0,0 +1,49 @@
+const cells = document.querySelectorAll(".cell"); 
+const statusText = document.querySelector("#StatusText");
+const restartBtn = document.getElementById("#RestartBtn");
+const winConditions= [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+let options = ["","","","","","","","",""]
+let currentPlayer = "X"
+let running = false;
+
+function initializeGame (){
+    cells.forEach(cell => cell.addEventListener("click", cellClicked) )
+    restartBtn.addEventListener("click", restartGame)
+    statusText.textContent = `${currentPlayer}'s turn`
+    running = true;
+}
+function cellClicked(){
+    const cellIndex = this.getAttribute("cellIndex");
+
+    if (options[cellIndex] != "" || !running) {
+        return
+    }
+    else
+        updateCell(this, cellIndex);
+}
+
+function updateCell(cell, index){
+
+}
+
+function changePlayer(){
+
+}
+
+function checkWinner(){
+
+}
+
+function restartGame(){
+
+}
